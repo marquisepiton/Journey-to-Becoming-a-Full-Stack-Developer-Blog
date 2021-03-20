0af53a1f76f2f4a4660442a2b948fece64bcf632
Marquise Piton <pitonmarquise@gmail.com>
2021-03-20 08:40:56 -0400

    added images and modified stles and index

---
+++ react/my-blog/src/App.js
@@ -0,0 +1,24 @@
+
+
+function App() {
+
+  let i = 0; 
+  return (
+    <div className="App">
+
+      {i}
+
+      {console.log('hello')}
+      {console.log(i)}
+
+      
+
+      Hello, World!
+
+      
+     
+    </div>
+  );
+}
+
+export default App;
